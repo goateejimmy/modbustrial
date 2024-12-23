b32088726e23755112a9a12cf541dd4a4a67a08c
Jim Kuo <Jim.Kuo@shl-group.com>
2024-12-23 13:50:36 +0800

    wait for first trial run

--- modbustrial/Form1.Designer.cs
+++ modbustrial/Form1.Designer.cs
@@ -30,18 +30,29 @@
 		{
 			this.port_comport = new System.Windows.Forms.ComboBox();
 			this.panel1 = new System.Windows.Forms.Panel();
-			this.label1 = new System.Windows.Forms.Label();
-			this.label2 = new System.Windows.Forms.Label();
-			this.port_status = new System.Windows.Forms.TextBox();
 			this.textBox1 = new System.Windows.Forms.TextBox();
-			this.port_connect = new System.Windows.Forms.Button();
+			this.port_status = new System.Windows.Forms.TextBox();
 			this.port_disconnect = new System.Windows.Forms.Button();
+			this.port_connect = new System.Windows.Forms.Button();
 			this.label3 = new System.Windows.Forms.Label();
-			this.modbus_address = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
 			this.panel2 = new System.Windows.Forms.Panel();
 			this.label4 = new System.Windows.Forms.Label();
+			this.panel3 = new System.Windows.Forms.Panel();
+			this.command_modeselect = new System.Windows.Forms.ComboBox();
+			this.command_recivedCommand = new System.Windows.Forms.TextBox();
+			this.command_currentCommand = new System.Windows.Forms.TextBox();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label8 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.panel4 = new System.Windows.Forms.Panel();
+			this.try_forcetesthaptic = new System.Windows.Forms.Button();
+			this.label7 = new System.Windows.Forms.Label();
 			this.panel1.SuspendLayout();
 			this.panel2.SuspendLayout();
+			this.panel3.SuspendLayout();
+			this.panel4.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// port_comport
@@ -68,34 +79,6 @@
 			this.panel1.Size = new System.Drawing.Size(527, 347);
 			this.panel1.TabIndex = 1;
 			// 
-			// label1
-			// 
-			this.label1.AutoSize = true;
-			this.label1.Font = new System.Drawing.Font("Book Antiqua", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label1.Location = new System.Drawing.Point(19, 10);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(206, 35);
-			this.label1.TabIndex = 1;
-			this.label1.Text = "Port connection";
-			// 
-			// label2
-			// 
-			this.label2.AutoSize = true;
-			this.label2.Location = new System.Drawing.Point(22, 54);
-			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(31, 15);
-			this.label2.TabIndex = 2;
-			this.label2.Text = "Port";
-			// 
-			// port_status
-			// 
-			this.port_status.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-			this.port_status.Location = new System.Drawing.Point(25, 291);
-			this.port_status.Name = "port_status";
-			this.port_status.ReadOnly = true;
-			this.port_status.Size = new System.Drawing.Size(479, 31);
-			this.port_status.TabIndex = 5;
-			// 
 			// textBox1
 			// 
 			this.textBox1.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
@@ -105,15 +88,14 @@
 			this.textBox1.TabIndex = 6;
 			this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
 			// 
-			// port_connect
+			// port_status
 			// 
-			this.port_connect.Location = new System.Drawing.Point(25, 236);
-			this.port_connect.Name = "port_connect";
-			this.port_connect.Size = new System.Drawing.Size(76, 39);
-			this.port_connect.TabIndex = 3;
-			this.port_connect.Text = "Connect";
-			this.port_connect.UseVisualStyleBackColor = true;
-			this.port_connect.Click += new System.EventHandler(this.port_connect_Click);
+			this.port_status.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.port_status.Location = new System.Drawing.Point(25, 291);
+			this.port_status.Name = "port_status";
+			this.port_status.ReadOnly = true;
+			this.port_status.Size = new System.Drawing.Size(479, 31);
+			this.port_status.TabIndex = 5;
 			// 
 			// port_disconnect
 			// 
@@ -125,6 +107,16 @@
 			this.port_disconnect.Text = "Disconnect";
 			this.port_disconnect.UseVisualStyleBackColor = true;
 			// 
+			// port_connect
+			// 
+			this.port_connect.Location = new System.Drawing.Point(25, 236);
+			this.port_connect.Name = "port_connect";
+			this.port_connect.Size = new System.Drawing.Size(76, 39);
+			this.port_connect.TabIndex = 3;
+			this.port_connect.Text = "Connect";
+			this.port_connect.UseVisualStyleBackColor = true;
+			this.port_connect.Click += new System.EventHandler(this.port_connect_Click);
+			// 
 			// label3
 			// 
 			this.label3.AutoSize = true;
@@ -134,18 +126,28 @@
 			this.label3.TabIndex = 2;
 			this.label3.Text = "Baudrate";
 			// 
-			// modbus_address
+			// label2
 			// 
-			this.modbus_address.Font = new System.Drawing.Font("新細明體", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-			this.modbus_address.Location = new System.Drawing.Point(35, 102);
-			this.modbus_address.Name = "modbus_address";
-			this.modbus_address.Size = new System.Drawing.Size(66, 40);
-			this.modbus_address.TabIndex = 2;
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(22, 54);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(31, 15);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "Port";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Book Antiqua", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(19, 10);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(206, 35);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Port connection";
 			// 
 			// panel2
 			// 
-			this.panel2.Controls.Add(this.label4);
-			this.panel2.Controls.Add(this.modbus_address);
+			this.panel2.Controls.Add(this.try_forcetesthaptic);
 			this.panel2.Location = new System.Drawing.Point(597, 40);
 			this.panel2.Name = "panel2";
 			this.panel2.Size = new System.Drawing.Size(1081, 346);
@@ -155,17 +157,121 @@
 			// 
 			this.label4.AutoSize = true;
 			this.label4.Font = new System.Drawing.Font("Book Antiqua", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label4.Location = new System.Drawing.Point(19, 10);
+			this.label4.Location = new System.Drawing.Point(19, 21);
 			this.label4.Name = "label4";
-			this.label4.Size = new System.Drawing.Size(320, 44);
+			this.label4.Size = new System.Drawing.Size(98, 35);
 			this.label4.TabIndex = 1;
-			this.label4.Text = "Modbus Command";
+			this.label4.Text = "Modes";
+			// 
+			// panel3
+			// 
+			this.panel3.Controls.Add(this.command_modeselect);
+			this.panel3.Controls.Add(this.label4);
+			this.panel3.Controls.Add(this.command_recivedCommand);
+			this.panel3.Controls.Add(this.command_currentCommand);
+			this.panel3.Controls.Add(this.label6);
+			this.panel3.Controls.Add(this.label8);
+			this.panel3.Controls.Add(this.label5);
+			this.panel3.Location = new System.Drawing.Point(48, 415);
+			this.panel3.Name = "panel3";
+			this.panel3.Size = new System.Drawing.Size(802, 455);
+			this.panel3.TabIndex = 4;
+			// 
+			// command_modeselect
+			// 
+			this.command_modeselect.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.command_modeselect.FormattingEnabled = true;
+			this.command_modeselect.Location = new System.Drawing.Point(25, 72);
+			this.command_modeselect.Name = "command_modeselect";
+			this.command_modeselect.Size = new System.Drawing.Size(261, 28);
+			this.command_modeselect.TabIndex = 2;
+			this.command_modeselect.SelectedIndexChanged += new System.EventHandler(this.command_modeselect_SelectedIndexChanged);
+			// 
+			// command_recivedCommand
+			// 
+			this.command_recivedCommand.Font = new System.Drawing.Font("新細明體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.command_recivedCommand.Location = new System.Drawing.Point(445, 182);
+			this.command_recivedCommand.Multiline = true;
+			this.command_recivedCommand.Name = "command_recivedCommand";
+			this.command_recivedCommand.ReadOnly = true;
+			this.command_recivedCommand.Size = new System.Drawing.Size(344, 257);
+			this.command_recivedCommand.TabIndex = 0;
+			// 
+			// command_currentCommand
+			// 
+			this.command_currentCommand.Font = new System.Drawing.Font("新細明體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.command_currentCommand.Location = new System.Drawing.Point(445, 70);
+			this.command_currentCommand.Name = "command_currentCommand";
+			this.command_currentCommand.ReadOnly = true;
+			this.command_currentCommand.Size = new System.Drawing.Size(344, 35);
+			this.command_currentCommand.TabIndex = 0;
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Font = new System.Drawing.Font("Book Antiqua", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label6.Location = new System.Drawing.Point(439, 144);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(265, 35);
+			this.label6.TabIndex = 1;
+			this.label6.Text = "Recieved Command";
+			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.Font = new System.Drawing.Font("Book Antiqua", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label8.Location = new System.Drawing.Point(19, 158);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(148, 35);
+			this.label8.TabIndex = 1;
+			this.label8.Text = "Set to Zero";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Font = new System.Drawing.Font("Book Antiqua", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label5.Location = new System.Drawing.Point(439, 21);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(215, 35);
+			this.label5.TabIndex = 1;
+			this.label5.Text = "Send Command";
+			// 
+			// panel4
+			// 
+			this.panel4.Controls.Add(this.label7);
+			this.panel4.Location = new System.Drawing.Point(877, 415);
+			this.panel4.Name = "panel4";
+			this.panel4.Size = new System.Drawing.Size(940, 454);
+			this.panel4.TabIndex = 5;
+			// 
+			// try_forcetesthaptic
+			// 
+			this.try_forcetesthaptic.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.try_forcetesthaptic.Location = new System.Drawing.Point(17, 54);
+			this.try_forcetesthaptic.Name = "try_forcetesthaptic";
+			this.try_forcetesthaptic.Size = new System.Drawing.Size(125, 54);
+			this.try_forcetesthaptic.TabIndex = 2;
+			this.try_forcetesthaptic.Text = "Constant Force Test ";
+			this.try_forcetesthaptic.UseVisualStyleBackColor = true;
+			this.try_forcetesthaptic.Click += new System.EventHandler(this.try_forcetesthaptic_Click);
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Font = new System.Drawing.Font("Book Antiqua", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label7.Location = new System.Drawing.Point(17, 21);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(249, 35);
+			this.label7.TabIndex = 1;
+			this.label7.Text = "Manual Command";
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(1831, 901);
+			this.Controls.Add(this.panel4);
+			this.Controls.Add(this.panel3);
 			this.Controls.Add(this.panel2);
 			this.Controls.Add(this.panel1);
 			this.Name = "Form1";
@@ -173,7 +279,10 @@
 			this.panel1.ResumeLayout(false);
 			this.panel1.PerformLayout();
 			this.panel2.ResumeLayout(false);
-			this.panel2.PerformLayout();
+			this.panel3.ResumeLayout(false);
+			this.panel3.PerformLayout();
+			this.panel4.ResumeLayout(false);
+			this.panel4.PerformLayout();
 			this.ResumeLayout(false);
 
 		}
@@ -189,9 +298,18 @@
 		private System.Windows.Forms.Button port_disconnect;
 		private System.Windows.Forms.Button port_connect;
 		private System.Windows.Forms.Label label3;
-		private System.Windows.Forms.TextBox modbus_address;
 		private System.Windows.Forms.Panel panel2;
 		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Panel panel3;
+		private System.Windows.Forms.TextBox command_currentCommand;
+		private System.Windows.Forms.TextBox command_recivedCommand;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.ComboBox command_modeselect;
+		private System.Windows.Forms.Panel panel4;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Button try_forcetesthaptic;
+		private System.Windows.Forms.Label label8;
 	}
 }
 
--- modbustrial/Form1.cs
+++ modbustrial/Form1.cs
@@ -9,33 +9,232 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO.Ports;
 
+
+
+
 namespace modbustrial
 {
 	public partial class Form1 : Form
 	{
+		private static Form1 form = null;
 
+		
+		SerialPort port;
 		//port settingstring[
-		SerialPort motorport;
 		string[] ports = SerialPort.GetPortNames();
 		int baudrate = 19200;
-		
+		int recievedDatalength;
+		public enum Mode
+		{
+			Sleep_Mode = 1,
+			Force_Mode = 2,
+			Position_Mode = 3,
+			Haptic_Mode = 4,
+			Kinematic_Mode = 5,
+		}
+
+		public enum Register
+		{
+			CTRL_REG_0 = 0,
+			CTRL_REG_1 = 1,
+			CTRL_REG_2 = 2,
+			CTRL_REG_3 = 3,
+
+			//Force control
+			FORCE_CMD = 28,
+			FORCE_CMD_H = 29, //upper two bytes 
+
+			//Position control
+			POS_CMD = 30,
+			POS_CMD_H = 31,
+
+			//Haptic control
+			HAPTIC_STATUS = 641,
+			CONSTANT_FORCE_MN = 642,
+			CONSTANT_FORCE_MN_H = 643,
+
+		}
+
+
+
 
 
 		public Form1()
 		{
 			InitializeComponent();
+			command_modeselect.Items.AddRange( Enum.GetNames(typeof(Mode)));
+			
 		}
 
 		private void port_connect_Click(object sender, EventArgs e)
 		{
-			motorport = new SerialPort(port_comport.Text, baudrate, Parity.Even, 8,StopBits.One);
+			port = new SerialPort(port_comport.Text, baudrate, Parity.Even, 8, StopBits.One);
+			port.DataReceived += Port_DataReceived;
+			//OrcaModbus =new Modbus();
+			//motorport.Write()
 			port_status.Text = "Conneted! Initial baudrate is 19200.";
+			command_modeselect.SelectedIndex = command_modeselect.FindStringExact("Sleep_Mode");
+
 
 		}
+
+		private void Port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+		{
+			if(port.BytesToRead >= recievedDatalength)
+			{
+				byte[] buffer = new byte[recievedDatalength];
+				port.Read(buffer, 0, buffer.Length);
+				string hex = BitConverter.ToString(buffer);
+				command_recivedCommand.AppendText(hex);
+				
+
+			}
+		}
+
+
 
 		private void textBox1_TextChanged(object sender, EventArgs e)
 		{
+			
+		}
 
+		private void command_modeselect_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			if(this.port != null)
+			{
+				Enum.TryParse((string)(command_modeselect.SelectedItem), out Mode curmode);
+				Setmode(curmode);
+			}
+		}
+
+		private void try_forcetesthaptic_Click(object sender, EventArgs e)
+		{
+			if (this.port != null)
+			{
+
+				HatpicConstant(2000); // set force to 2 N
+
+			}
+		}
+
+
+		public void HatpicConstant(int forcemn)
+
+		{
+			this.Setmode(Mode.Haptic_Mode); // need to switch to haptic mode. need to find better way to look up whether the mode is in haptic mode right now
+			this.WriteSingleRegister((ushort)Register.HAPTIC_STATUS, (ushort)1); //only open the constant function, see page 42
+			this.WriteTwoRegister((ushort)Register.CONSTANT_FORCE_MN, forcemn);
+
+		}
+		public void Setmode(Mode mode)
+		{
+			this.WriteSingleRegister((ushort)Register.CTRL_REG_3, (ushort)mode);
+		}
+
+		public void ReadHoldingRegister(ushort startAddress, ushort quantity)
+		{
+			recievedDatalength = 5 + 2 * quantity;
+
+			// Modbus Command Structure (Example for Read)
+			// | Device ID | Function Code | Start Address | Quantity | CRC |
+			byte[] command = packet8byte(3, startAddress, quantity);
+			command_currentCommand.Text = BitConverter.ToString(command);
+			this.port.Write(command, 0, command.Length);
+
+		}
+
+		public void WriteSingleRegister(ushort writeAddress, ushort writevalue)
+		{
+			recievedDatalength = 8;
+			byte[] command = packet8byte(6, writeAddress, writevalue);
+			command_currentCommand.Text = BitConverter.ToString(command);
+			this.port.Write(command, 0, command.Length);
+
+
+		}
+
+		//I think there will be a function pointer that can adjust the command with different number of register
+		public void WriteTwoRegister(ushort startingAddress, int continuousvalue)
+		{
+			recievedDatalength = 8;
+			byte[] command = new byte[14];
+			command[0] = (byte)0X01;
+			command[1] = (byte)0X10;
+
+			//Start register
+			command[2] = (byte)(startingAddress >> 8); // High byte
+			command[3] = (byte)(startingAddress & 0xFF); // Low byte
+
+			//Two register to write
+			command[4] = (byte)0X00;
+			command[5] = (byte)0X02;
+
+
+			//byte count
+			command[6] = (byte)0X00;
+			command[7] = (byte)0X04;// 2*2 
+
+
+			//write. low byte first and then high byte
+			command[8] = (byte)((continuousvalue >> 8) & 0xFF);   // 低位 16 位元的高字節 (D4)
+			command[9] = (byte)((continuousvalue >> 0) & 0xFF);   // 低位 16 位元的低字節 (C0)
+			command[10] = (byte)((continuousvalue >> 24) & 0xFF);  // 高位 16 位元的高字節 (00)
+			command[11] = (byte)((continuousvalue >> 16) & 0xFF);  // 高位 16 位元的低字節 (01)
+
+
+			ushort crc = ComputeCRC(command, 12);
+			command[12] = (byte)(crc >> 8); // CRC High byte
+			command[13] = (byte)(crc & 0xFF); // CRC Low byte
+
+			command_currentCommand.Text = BitConverter.ToString(command);
+			this.port.Write(command, 0, command.Length);
+		}
+		public byte[] packet8byte(byte func, ushort address, ushort cmd)
+		{
+			byte[] command = new byte[8];
+			command[0] = (byte)0x01; // Address
+			command[1] = func; // Function Code
+
+			// Start Address (Low Byte, High Byte)
+			command[2] = (byte)(address >> 8);   // High byte
+			command[3] = (byte)(address & 0xFF); // Low byte
+
+
+			// Quantity (Low Byte, High Byte)
+			command[4] = (byte)(cmd >> 8);   // High byte
+			command[5] = (byte)(cmd & 0xFF); // Low byte
+
+
+			// Compute and add CRC (Cyclic Redundancy Check)
+			ushort crc = ComputeCRC(command, 6);
+			command[6] = (byte)(crc >> 8);   // CRC High byte
+			command[7] = (byte)(crc & 0xFF); // CRC Low byte
+
+
+			return command;
+		}
+
+		private static ushort ComputeCRC(byte[] data, int length)
+		{
+			ushort crc = 0xFFFF;
+			for (int i = 0; i < length; i++)
+			{
+				crc ^= data[i];
+				for (int j = 0; j < 8; j++)
+				{
+					if ((crc & 0x0001) != 0)
+					{
+						crc >>= 1;
+						crc ^= 0xA001;
+					}
+					else
+					{
+						crc >>= 1;
+					}
+				}
+			}
+			return crc;
 		}
 	}
 }
+
